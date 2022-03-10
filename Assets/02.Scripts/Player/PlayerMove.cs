b9f0e5b7b53cb68595d21c0a56590a68168b7f3a
yj3677 <dudwn3677@gmail.com>
2022-03-10 15:49:36 +0900

    20220310

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -5,19 +5,36 @@ using UnityEngine.EventSystems;
 
 public class PlayerMove : MonoBehaviour,IBeginDragHandler,IDragHandler,IEndDragHandler
 {
+    [SerializeField]
+    private RectTransform stickCircle;
+    private RectTransform rectTramsform;
+
+    [SerializeField,Range(-20,360)]
+    float stickCircleRange;
+    private void Awake()
+    {
+        rectTramsform = GetComponent<RectTransform>();
+    }
     public void OnBeginDrag(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
+        var inputPos = eventData.position - rectTramsform.anchoredPosition;
+        var inputVector = inputPos.magnitude < stickCircleRange ? inputPos : inputPos.normalized * stickCircleRange;
+        stickCircle.anchoredPosition = inputVector;
+        Debug.Log("Begin");
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
+        var inputPos = eventData.position - rectTramsform.anchoredPosition;
+        var inputVector = inputPos.magnitude < stickCircleRange ? inputPos : inputPos.normalized * stickCircleRange;
+        stickCircle.anchoredPosition = inputVector;
+        Debug.Log("Drag");
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
+        stickCircle.anchoredPosition = new Vector3(0,0,0);
+        Debug.Log("End");
     }
 
     // Start is called before the first frame update
