05321c114a166d3aecdd3ccc0cf5407df36940fe
yj3677 <dudwn3677@gmail.com>
2022-03-17 15:45:26 +0900

    20220317

--- Assets/02.Scripts/Enemy/EnemyAI.cs
+++ Assets/02.Scripts/Enemy/EnemyAI.cs
@@ -1,20 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-
+/// <summary>
+/// 일정시간 지나면 AI가 플레이어에게 관심X or 안움직임
+/// </summary>
 public class EnemyAI : MonoBehaviour
 {
     public NavMeshAgent agent;
     public Transform player;
     public LayerMask isGround, isPlayer;
+    Animator anim;
 
     public float health;
 
     //Patroling
     public Vector3 walkPoint;
-    bool walkPointSet;
+    public bool walkPointSet;
     public float walkPointRange;
+    float currentIdle;
+
 
     //Attacking
     public float timeBetweenAttacks;
@@ -28,17 +31,28 @@ public class EnemyAI : MonoBehaviour
     private void Awake()
     {
         player = GameObject.Find("Player").transform;
+        anim = GetComponentInChildren<Animator>();
         agent = GetComponent<NavMeshAgent>();
+    }
+    private void OnEnable()
+    {
+        AIRangeCheck();
     }
     private void Update()
     {
-        //Check for sight and attack range
-        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, isPlayer);
-        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, isPlayer);
+       AIRangeCheck();
+    }
 
+    void AIRangeCheck()
+    {
+        //Check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, isPlayer);  //시야범위
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, isPlayer); //공격범위
+        Debug.Log(playerInAttackRange);
         if (!playerInSightRange && !playerInAttackRange)
         {
             Patroling();
+            
         }
         if (playerInSightRange && !playerInAttackRange)
         {
@@ -48,69 +62,93 @@ public class EnemyAI : MonoBehaviour
         {
             AttackPlayer();
         }
+        
 
 
     }
     void Patroling()
     {
-        if (!walkPointSet)
+
+        if (!walkPointSet) //false면 이동할 좌표 구해라
         {
             SearchWalkPoint();
         }
         if (walkPointSet)
         {
-            agent.SetDestination(walkPoint);
+            agent.SetDestination(walkPoint); //목적지로 이동
         }
-        Vector3 distanceToWalkPoint = transform.position - walkPoint;
-
+        Vector3 distanceToWalkPoint = transform.position - walkPoint; //목적지로 이동
+        AiIdle();
         //Walkpoint reached
-        if (distanceToWalkPoint.magnitude<1)
+        if (distanceToWalkPoint.magnitude < 1)
         {
             walkPointSet = false;
+            Invoke("WalkPointReset", 10); //순찰 반복
         }
+
     }
     void SearchWalkPoint()
     {
         float randomZ = Random.Range(-walkPointRange, walkPointRange);
         float randomX = Random.Range(-walkPointRange, walkPointRange);
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);  //AI랜덤이동
+        
+        walkPointSet = true;
 
-        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
-
-        if (Physics.Raycast(walkPoint,-transform.up,2,isGround))
+    }
+    void AiIdle() //10초가 되면 멈추기
+    {
+        currentIdle += Time.deltaTime;
+        if (currentIdle >= 10)
         {
-            walkPointSet = true;
+            anim.SetBool("IsWalk", false);
+            agent.speed = 0;
+            currentIdle = 0;
+            Invoke("AiMove", 5);
         }
+    }
+    void AiMove()
+    {
+        agent.speed = 2;
+    }
+    void WalkPointReset()
+    {
+        walkPointSet = !walkPointSet;
+
     }
     void ChasePlayer()
     {
-        agent.SetDestination(player.position);
+        agent.SetDestination(player.position); 
     }
     void AttackPlayer()
     {
-        //Make sure enemy doesn't move
         agent.SetDestination(transform.position);
         transform.LookAt(player);
+        
         if (!alreadyAttacked)
         {
+            anim.SetBool("IsWalk", false);
             //Attack 
-            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();  //공격체 생성
             rb.AddForce(transform.forward * 32, ForceMode.Impulse);
             rb.AddForce(transform.up * 8, ForceMode.Impulse);
+            
 
             alreadyAttacked = true;
-            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+            Invoke("ResetAttack", timeBetweenAttacks);  //공격시간
         }
     }
     void ResetAttack()
     {
         alreadyAttacked = false;
     }
+
     public void TakeDamage(int damage)
     {
         health -= damage;
-        if (health<=0)
+        if (health <= 0)
         {
-            Invoke(nameof(DestroyEnemy),0.5f);
+            Invoke("DestroyEnemy", 0.5f);
         }
     }
     void DestroyEnemy()
@@ -124,8 +162,4 @@ public class EnemyAI : MonoBehaviour
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, sightRange);
     }
-
-
-
-
 }
