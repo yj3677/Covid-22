b4940976564fa46fa70f9c349b05489e060b55b2
yj3677 <dudwn3677@gmail.com>
2022-03-21 20:20:50 +0900

    20220321

--- Assets/02.Scripts/Enemy/EnemyAI.cs
+++ Assets/02.Scripts/Enemy/EnemyAI.cs
@@ -1,9 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 /// <summary>
-/// 일정시간 지나면 AI가 플레이어에게 관심X or 안움직임
+/// 공격거리, 쫓아가는 거리, 죽었는지 유무 판단
 /// </summary>
 public class EnemyAI : MonoBehaviour
 {
-    
+    public enum State
+    {
+        PATROL, TRACE, ATTACK, DIE
+    }
+    public State state = State.PATROL;
+    private Transform playerTr;
+    private Transform enemyTr;
+
+    public float attackDist = 5;
+    public float traceDis = 10;
+    public bool isDie = false;
+
+    private WaitForSeconds waitForSeconds;
+
+    private void Awake()
+    {
+        var player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player !=null)
+        {
+            playerTr = player.GetComponent<Transform>();
+            enemyTr = GetComponent<Transform>();
+            waitForSeconds = new WaitForSeconds(0.3f);
+        }
+    }
+    private void OnEnable()
+    {
+        StartCoroutine(CheckState());
+
+    }
+    IEnumerator CheckState()
+    {
+        while(!isDie)
+        {
+            if (state == State.DIE) 
+            {
+                yield break;
+            }
+            float dist = Vector3.Distance(playerTr.position, enemyTr.position);
+            
+            if (dist<=attackDist)
+            {
+                state = State.ATTACK;
+            }
+            else if (dist<=traceDis)
+            {
+                state = State.TRACE;
+            }
+            else
+            {
+                state = State.PATROL;
+            }
+            yield return waitForSeconds;
+        }
+    }
 }
